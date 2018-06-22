4f969cb89eb4689e6851c64fd8c8706a52b7a056
Nam Anh <namanhchu2103@gmail.com>
2018-06-22 23:10:24 +0700

    Add firebase

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,36 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter as Router} from "react-router-dom"
+import * as firebase from "firebase"
 
 
 class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      speed: 10,
+    }
+  }
+
+  // componentDidMount() {
+  //   const rootRef = firebase.database().ref().child("react")
+  //   const speedRef = rootRef.child("speed")
+  //   speedRef.on("value", snap => {
+  //     this.setState({
+  //       speed: snap.val(),
+  //     })
+  //   })
+
+    
+  // }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React with speed: {this.state.speed}</h1>
           
         </header>
         <p className="App-intro">
